faff2b7d05ff0f436480ae5122dd1aafb2a65144
Jaymes Lightfoot <157078893+ToriALaMode@users.noreply.github.com>
2024-05-01 04:11:28 +0000

    Homework 9

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,11 @@
-#
+import random
+
+class Die:
+    def roll(self):
+        self.roll_value = random.randint(1, 6)
+    
+    def get_rolled_value(self):
+        return self.roll_value
+    
+    def __str__(self):
+        return "The rolled value is {}".format(self.roll_value)
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,12 @@
-#
+import unittest
+from j_classes import Die
+class TestDie(unittest.TestCase):
+    def test_rolled_value_range(self):
+        die = Die()
+        for _ in range(3):
+            die.roll()  # Roll the die
+            rolled_value = die.get_rolled_value()  # Get the rolled value
+            self.assertTrue(1 <= rolled_value <= 6, "Rolled value is out of range")
+
+if __name__ == '__main__':
+    unittest.main()
